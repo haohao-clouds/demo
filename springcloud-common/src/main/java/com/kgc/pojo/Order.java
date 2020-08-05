616dada476341926483d5b9412c6edd3763dd900
haohao <zfc15190757398@163.com>
2020-08-05 17:08:43 +0800

    master springcloud-common

---
+++ springcloud-common/src/main/java/com/kgc/pojo/Order.java
@@ -0,0 +1,42 @@
+package com.kgc.pojo;
+
+import java.io.Serializable;
+
+public class Order {
+    private Integer id;
+    private String title;
+    private Double price;
+    private Integer uid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+}
